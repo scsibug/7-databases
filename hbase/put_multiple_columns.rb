e692958dda212fb013c12a74074df92dff25c227
Greg Heartsfield <scsibug@imap.cc>
2012-08-04 19:33:32 -0500

    HBase day 1 completed.

---
+++ hbase/put_multiple_columns.rb
@@ -0,0 +1,16 @@
+import 'org.apache.hadoop.hbase.client.HTable'
+import 'org.apache.hadoop.hbase.client.Put'
+
+def jbytes ( *args )
+  args.map { |arg| arg.to_s.to_java_bytes }
+end
+
+table = HTable.new (@hbase.configuration, "wiki")
+
+p = Put.new(*jbytes("Home"))
+
+p.add(*jbytes("text", "", "Hello World"))
+p.add(*jbytes("revision", "author", "jimbo"))
+p.add(*jbytes("revision", "comment", "my first edit"))
+
+table.put(p)
